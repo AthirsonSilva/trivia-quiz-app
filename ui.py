6487e33deeec6f188bfaf372ba6c0fd6abad2373
Athirson <Athirsonarceus@gmail.com>
2022-04-22 15:57:29 -0300

    Graphical user interface

--- ui.py
+++ ui.py
@@ -18,9 +18,19 @@ class QuizInterface:
         self.score_label = tkinter.Label(
             text='Score: 0', fg='white', bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
-        self.canvas = tkinter.Canvas(width=300, height=250, bg='white')
 
-        self.question_text = self.canvas.create_text(150, 125, text='Some question text', fill=THEME_COLOR)
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.canvas = tkinter.Canvas(width=300, height=250, bg='white')
+        self.question_text = self.canvas.create_text(
+            150, 125, text='Some question text', fill=THEME_COLOR, font=('Arial', 20, 'italic'))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = tkinter.PhotoImage(
+            file='projects/trivia-quiz-app/images/true.png')
+        false_image = tkinter.PhotoImage(
+            file='projects/trivia-quiz-app/images/false.png')
+        self.true_button = tkinter.Button(image=true_image, highlightthickness=0)
+        self.false_button = tkinter.Button(image=false_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+        self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
